896b8946c146ab5f8672372731ec2d1aa9b5316f
Kleo Davidson <faceplatemusic@gmail.com>
2024-02-16 11:05:48 -0700

    feat: add lua_ls to lsp configuration

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -52,3 +52,28 @@ require('lspconfig')['gopls'].setup{}
 require('lspconfig')['hls'].setup{}
 
 require('lspconfig')['solargraph'].setup{}
+
+require('lspconfig')['lua_ls'].setup{
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                    runtime = {
+                        version = 'LuaJIT'
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        }
+                    }
+                }
+            })
+
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end
+        return true
+    end
+}
+
